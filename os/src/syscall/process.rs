7654a424c33dce3d8a64b0a835f5d36aa50023ed
creatoy <creatoy@yeah.net>
2025-03-30 22:13:53 +0800

    lab1

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 //! Process management syscalls
 use crate::{
-    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    task::{exit_current_and_run_next, suspend_current_and_run_next, TASK_MANAGER},
     timer::get_time_us,
 };
 
@@ -38,8 +38,16 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
-// TODO: implement the syscall
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+/// get trace info
+pub fn sys_trace(trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match trace_request {
+        0 => unsafe { (_id as *const u8).read() as isize },
+        1 => unsafe {
+            (_id as *mut u8).write(_data as u8);
+            0
+        },
+        2 => TASK_MANAGER.get_syscall_times(_id) as isize,
+        _ => -1,
+    }
 }
